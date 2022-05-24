65fb531a34025d0bd76f85b6aa1606af29b77384
Valoriaz <lolcarry7@gmail.com>
2022-05-24 11:35:58 +0200

    Update Solocraft.sql
    from tinyint to bigint ( every Character with GUID <255 can scale endless)

--- data/sql/db-characters/Solocraft.sql
+++ data/sql/db-characters/Solocraft.sql
@@ -1,6 +1,6 @@
 -- Structure for table custom_solocraft_character_stats
 CREATE TABLE IF NOT EXISTS `custom_solocraft_character_stats` (
-  `GUID` tinyint unsigned NOT NULL,
+  `GUID` bigint unsigned NOT NULL,
   `Difficulty` float NOT NULL,
   `GroupSize` int NOT NULL,
   `SpellPower` int unsigned NOT NULL DEFAULT '0',
